3da70f97b7ca8a1685dfd7d8508778488c18c9ab
dylanzepickle <dylankstreat@gmail.com>
2024-11-20 22:06:54 -0500

    Added masonry layout

--- website/myapp/urls.py
+++ website/myapp/urls.py
@@ -3,8 +3,10 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='gallery'),
-    path('gallery', views.gallery, name='gallery')
+    path('', views.home, name='home'),
+    path('gallery', views.gallery, name='gallery'),
+    path('about', views.about, name='about'),
+    path('faq', views.faq, name='faq'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- website/myapp/views.py
+++ website/myapp/views.py
@@ -2,15 +2,27 @@ import os
 from django.conf import settings
 from django.shortcuts import render, HttpResponse
 
+image_extensions = ('.png', '.jpg', '.jpeg', '.gif', '.webp')
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def gallery(request):
     # path to image directory
-    images_dir = os.path.join(settings.STATICFILES_DIRS[0], 'images')
+    images_dir = os.path.join(settings.BASE_DIR, 'myapp', 'assets', 'images', 'gallery')
+    print(f'Image directory: {images_dir}')
 
-    # list all image files
-    images = [f'images{file}' for file in os.listdir(images_dir) if file.endswith(('.jpg', '.png'))]
+    image_list = [
+        f'images/gallery/{filename}' for filename in os.listdir(images_dir) if filename.lower().endswith(image_extensions)
+    ]
+    print(f'Image list:\n{image_list}')
 
-    return render(request, 'gallery.html')
+    return render(request, 'gallery.html', {'images': image_list})
+
+def about(request):
+    return render(request, 'about.html')
+
+def faq(request):
+    return render(request, 'faq.html')
+
